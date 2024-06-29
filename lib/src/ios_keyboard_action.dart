222da38eddf71e04ba5f50af22cf6f46ba2d953d
4u1514g <76207064+4u1514g@users.noreply.github.com>
2024-06-29 15:15:20 +0700

    Update ios_keyboard_action.dart

--- lib/src/ios_keyboard_action.dart
+++ lib/src/ios_keyboard_action.dart
@@ -11,6 +11,7 @@ class IOSKeyboardAction extends StatefulWidget {
   final Widget? child;
   final VoidCallback? onTap;
   final String? label;
+  final VoidCallback? callBack;
 
   ///Creates a IOSKeyboardAction that can be used to your text field to add a keyboard action above the system keyboard.
   ///
@@ -20,15 +21,15 @@ class IOSKeyboardAction extends StatefulWidget {
   ///The [focusNode] is the focus node that will be used to determine if the keyboard is visible.
   ///The [child] is the child widget that will be wrapped with the IOSKeyboardAction.
   ///The [onTap] is the callback that will be called when the user taps the action button.
-  const IOSKeyboardAction(
-      {Key? key,
-      required this.focusNode,
-      this.backgroundColor = const Color(0xffeeeeed),
-      this.child,
-      this.focusActionType = FocusActionType.done,
-      this.onTap,
-      this.textColor = Colors.black,
-      this.label})
+  const IOSKeyboardAction({Key? key,
+    required this.focusNode,
+    this.backgroundColor = const Color(0xffeeeeed),
+    this.child,
+    this.focusActionType = FocusActionType.done,
+    this.onTap,
+    this.callBack,
+    this.textColor = Colors.black,
+    this.label})
       : super(key: key);
 
   @override
@@ -40,49 +41,53 @@ class _IOSKeyboardActionState extends State<IOSKeyboardAction> {
 
   OverlayEntry _createOverlayEntry() {
     return OverlayEntry(
-      builder: (context) => Align(
-        alignment: Alignment.bottomCenter,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.end,
-          children: [
-            Container(
-              height: 50,
-              width: double.infinity,
-              color: widget.backgroundColor,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  const Padding(
-                    padding: EdgeInsets.symmetric(vertical: 8.0),
-                    child: VerticalDivider(
-                      color: Colors.black,
-                    ),
+      builder: (context) =>
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Container(
+                  height: 50,
+                  width: double.infinity,
+                  color: widget.backgroundColor,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      const Padding(
+                        padding: EdgeInsets.symmetric(vertical: 8.0),
+                        child: VerticalDivider(
+                          color: Colors.black,
+                        ),
+                      ),
+                      TextButton(
+                        onPressed: () {
+                          Controller.getFunctionAction(
+                              action: widget.focusActionType,
+                              focusNode: widget.focusNode);
+                          if (widget.onTap != null) {
+                            widget.onTap!();
+                          }
+                        },
+                        child: Text(
+                          widget.label ??
+                              Controller.getActionTypeLabel(widget.focusActionType),
+                          style: TextStyle(color: widget.textColor),
+                        ),
+                      )
+                    ],
                   ),
-                  TextButton(
-                    onPressed: () {
-                      Controller.getFunctionAction(
-                          action: widget.focusActionType,
-                          focusNode: widget.focusNode);
-                      if (widget.onTap != null) {
-                        widget.onTap!();
-                      }
-                    },
-                    child: Text(
-                      widget.label ??
-                          Controller.getActionTypeLabel(widget.focusActionType),
-                      style: TextStyle(color: widget.textColor),
-                    ),
-                  )
-                ],
-              ),
+                ),
+                AnimatedContainer(
+                  duration: const Duration(milliseconds: 500),
+                  height: MediaQuery
+                      .of(context)
+                      .viewInsets
+                      .bottom,
+                )
+              ],
             ),
-            AnimatedContainer(
-              duration: const Duration(milliseconds: 500),
-              height: MediaQuery.of(context).viewInsets.bottom,
-            )
-          ],
-        ),
-      ),
+          ),
     );
   }
 
@@ -93,6 +98,9 @@ class _IOSKeyboardActionState extends State<IOSKeyboardAction> {
         if (widget.focusNode.hasFocus == true) {
           _overlayEntry = _createOverlayEntry();
           Overlay.of(context)!.insert(_overlayEntry!);
+          if (widget.callBack != null) {
+            widget.callBack();
+          }
         } else {
           _overlayEntry!.remove();
         }
